54bd8e29fa9d4dfbf0bfe41bda19bdb79070a8f6
baudu <bastien.audu@gmail.com>
2021-06-08 15:57:45 +0200

    liste des vaccins

--- Centre/views.py
+++ Centre/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
 
 from Centre.Serializer import VaccinSerializer
 from Centre.models import Vaccin
@@ -24,11 +25,20 @@ def vaccin_list(request):
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def vaccin_detail(request, pk):
-    try:
-        unVaccin = Vaccin.objects.get(pk=pk)
-        vaccin_serializer = VaccinSerializer(unVaccin)
-        return JsonResponse(vaccin_serializer.data, safe=False)
-    except Vaccin.DoesNotExist:
-        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
-
-        # GET / PUT / DELETE tutorial
+    unVaccin = Vaccin.objects.get(pk=pk)
+    if request.method == 'GET':
+        try:
+            vaccin_serializer = VaccinSerializer(unVaccin)
+            return JsonResponse(vaccin_serializer.data, safe=False)
+        except Vaccin.DoesNotExist:
+            return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':
+        vaccin_data = JSONParser().parse(request)
+        vaccin_serializer = VaccinSerializer(unVaccin, data=vaccin_data)
+        if vaccin_serializer.is_valid():
+            vaccin_serializer.save()
+            return JsonResponse(vaccin_serializer.data)
+        return JsonResponse(vaccin_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        unVaccin.delete()
+        return JsonResponse({'message': 'Tutorial was deleted successfully!'}, status=status.HTTP_204_NO_CONTENT)
